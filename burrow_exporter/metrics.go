bad384f491977c0a68f3df1947902adf011adc11
Nawfal Shammari <Nawfal.Shammari@netent.com>
2019-09-23 15:55:33 +0200

    Init commit

---
+++ burrow_exporter/metrics.go
@@ -0,0 +1,57 @@
+package burrow_exporter
+
+import "github.com/prometheus/client_golang/prometheus"
+
+var (
+	KafkaConsumerPartitionLag = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "kafka_burrow_partition_lag",
+			Help: "The lag of the latest offset commit on a partition as reported by burrow.",
+		},
+		[]string{"cluster", "group", "topic", "partition"},
+	)
+	KafkaConsumerPartitionCurrentOffset = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "kafka_burrow_partition_current_offset",
+			Help: "The latest offset commit on a partition as reported by burrow.",
+		},
+		[]string{"cluster", "group", "topic", "partition"},
+	)
+	KafkaConsumerPartitionMaxOffset = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "kafka_burrow_partition_max_offset",
+			Help: "The log end offset on a partition as reported by burrow.",
+		},
+		[]string{"cluster", "group", "topic", "partition"},
+	)
+	KafkaConsumerTotalLag = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "kafka_burrow_total_lag",
+			Help: "The total amount of lag for the consumer group as reported by burrow",
+		},
+		[]string{"cluster", "group"},
+	)
+	KafkaTopicPartitionOffset = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "kafka_burrow_topic_partition_offset",
+			Help: "The latest offset on a topic's partition as reported by burrow.",
+		},
+		[]string{"cluster", "topic", "partition"},
+	)
+	KafkaActiveBrokers = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "kafka_active_brokers",
+			Help: "Number of active brokers as reported by burrow.",
+		},
+		[]string{"cluster"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(KafkaConsumerPartitionLag)
+	prometheus.MustRegister(KafkaConsumerPartitionCurrentOffset)
+	prometheus.MustRegister(KafkaConsumerPartitionMaxOffset)
+	prometheus.MustRegister(KafkaConsumerTotalLag)
+	prometheus.MustRegister(KafkaTopicPartitionOffset)
+	prometheus.MustRegister(KafkaActiveBrokers)
+}
